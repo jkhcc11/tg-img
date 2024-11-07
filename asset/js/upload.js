24b0ebaf8e2b500b7ec74bcb894b1b245f98cc05
Fish <137651076@qq.com>
2024-11-07 16:45:17 +0800

    fix upload

--- asset/js/upload.js
+++ asset/js/upload.js
@@ -234,7 +234,7 @@ $smfile.on('fileuploaded', function (event, data, previewId, index) {
     var form = data.form, files = data.files, extra = data.extra, response = data.response, reader = data.reader;
     // console.log(files);
     if (response.error === undefined) {
-        response = { data: { url: response[0].src } }
+        response = { data: { url: response.src } }
         // console.log(response);
         uploaded_files.push({ index: index, resp: response });
         render_uploaded();
